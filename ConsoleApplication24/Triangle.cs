d385e3a44280f9f00e624b81c063fd03203f0dae
Дима <@->
2017-02-21 15:51:01 +0500

    2

---
+++ ConsoleApplication24/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class Triangle
+    {
+        public Point A;
+        public Point B;
+        public Point C;
+
+        public Triangle(Point a, Point b, Point c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
---
+++ ConsoleApplication24/edge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class edge
+    {
+        public Point A;
+        public Point B;
+        public double Dlina;
+        public edge(Point a, Point b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public void GetLength()
+        {
+            Dlina = //формула длины отрезка;
+        }
+    }
+}
